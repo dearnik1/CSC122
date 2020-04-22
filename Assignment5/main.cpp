55591aaafc92026c2f6a8822663a7fbf2402323f
NikolasGT <50718085+NikolasGT@users.noreply.github.com>
2020-04-22 16:02:52 -0500

    Update main.cpp

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -10,14 +10,14 @@ int main() {
     cin >> h >> m >> s;
     t1.setTime(h, m, s);
     cout << "t1 is "; 
-    t1.getTime(t1);
+    t1.getTime();
    
     //object 2
     cout << "Enter a time(h m s): ";
     cin >> h >> m >> s;
     Time t2(h, m, s);
     cout << "t2 is ";
-    t1.getTime(t2);
+    t1.getTime();
 
     if (t1 == t2)
         cout << "Time t1 is equal to t2" << endl;
@@ -28,6 +28,6 @@ int main() {
 
     Time t3 = t1 + t2;
     cout << "t3(t1+t2): ";
-    t3.getTime(t3);
+    t3.getTime();
     return 0;
 }
