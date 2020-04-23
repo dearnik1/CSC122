c425cd80bc6eed2ac7f2dc4cb473b43eb2e880da
NikolasGT <50718085+NikolasGT@users.noreply.github.com>
2020-04-23 13:52:03 -0500

    Update Time.cpp

--- Assignment5/Time.cpp
+++ Assignment5/Time.cpp
@@ -8,13 +8,13 @@ Time::Time(int h, int m, int s) {
 }
 
 void Time::setTime(int h, int m, int s) {
-    t.hours = h;
-    t.minutes = m;
-    t.seconds = s;
+    t.hours = (h + m / 60 + s / 3600) % 24;	// Hours would never be > 23 in HH:MM:SS format
+    t.minutes = (m + s / 60) % 60;			// Take into account that 10h > 25h because 25h would turn into 1h
+    t.seconds = s % 60;
 }
-void Time::getTime()const {
-    cout << t.hours << ":" << t.minutes
-        << ":" << t.seconds << endl;
+
+timeStructure Time::getTime()const {
+    return t;
 }
 
 //overloading operators
@@ -42,7 +42,20 @@ Time Time::operator+(const Time& time) const {
 bool Time::operator==(const Time& time) const {
     return((t.hours == time.t.hours) && (t.minutes == time.t.minutes) && (t.seconds == time.t.seconds));
 }
+
 bool Time::operator>(const Time& time) const {
-    return((t.hours > time.t.hours) || ((t.hours == time.t.hours) && (t.minutes > time.t.minutes))
-        || ((t.hours == time.t.hours) && (t.minutes == time.t.minutes) && (t.seconds > time.t.seconds)));
+    return(t.hours * 3600 + t.minutes * 60 + t.seconds > time.t.hours * 3600 + time.t.minutes * 60 + time.t.seconds);
+    // converting into seconds is easier than building a whole logic comparing hours, minutes and seconds
+}
+
+void Time::printTime(timeStructure t) {	// output nice & neat HH:MM:SS format
+    if (t.hours < 10)
+        cout << 0;
+    cout << t.hours << ":";
+    if (t.minutes < 10)
+        cout << 0;
+    cout << t.minutes << ":";
+    if (t.seconds < 10)
+        cout << 0;
+    cout << t.seconds << endl;
 }
