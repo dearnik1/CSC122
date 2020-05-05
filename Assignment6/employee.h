ef92851beb2188d551f22a19093825df0127604e
NikolasGT <50718085+NikolasGT@users.noreply.github.com>
2020-05-05 03:17:21 -0500

    Create employee.h

---
+++ Assignment6/employee.h
@@ -0,0 +1,31 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class employee {
+private:
+    int number, age;
+    string name, education;
+    char gender;
+    
+    static int managerNum, workerNum;
+public:
+    employee();
+    //sets
+    void setNumber(int);
+    void setName(string);
+    void setAge(int);
+    void setGender(char);
+    void setEducation(string);
+    //gets
+    int getNumber() { return number; }
+    string getName() { return name; }
+    int getAge() { return age; }
+    char getGender() { return gender; }
+    string getEducation() { return education; }
+};
+
+#endif
