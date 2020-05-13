bbf3a332cb81277c4a66207a9ec48e792d1c5d16
lyang-zero <lyang.zero@gmail.com>
2020-05-12 19:33:33 -0500

    fix7.1

---
+++ Assignment7/HourlyImp.cpp
@@ -0,0 +1,52 @@
+#include "Hourly.h"
+
+Hourly::Hourly() : Employee()
+{
+	hours = 40;
+	wage = 20;
+	category = "full time";
+}
+Hourly::Hourly(string fName, string lName, char gen, int dep, Benefit ben,
+	double wage, double hours, string category) : Employee(fName, lName, gen, dep, ben)
+{
+	setWage(wage);
+	setHours(hours);
+	setCategory(category);
+}
+Hourly::Hourly(double wage, double hours, string category) : Employee()
+{
+	setWage(wage);
+	setHours(hours);
+	setCategory(category);
+}
+void Hourly::setAnnualSalary()
+{
+	annualSalary = calculatePay()*52;
+}
+void Hourly::setWage(double _wage)
+{
+	if(MAX_WAGE>=_wage>=MIN_WAGE)
+		wage = _wage;
+}
+void Hourly::setHours(double _hours)
+{
+	if(MAX_HOURS>=_hours>=MIN_HOURS)
+		hours = _hours;
+}
+void Hourly::setCategory(string cat)
+{
+	if(cat == "temporary" || cat == "part time" ||cat == "full time")
+		category = cat;
+}
+double Hourly::calculatePay()
+{
+	return wage * hours;
+}
+void Hourly::displayEmployee()
+{
+	Employee::displayEmployee();
+	cout << setw(22) << left << "Category: " << getCat() << endl;;
+	cout << setw(22) << left << "Wage: " << getWage() << endl;;
+	cout << setw(22) << left << "Hours: " << getHours() << endl;;
+	cout << "---------Number of Objects created: " << getNumEmployees() << "---------" << endl << endl;
+}
---
+++ Assignment7/main.cpp
@@ -0,0 +1,82 @@
+#include "Employee.h"
+#include "Salaried.h"
+#include "Hourly.h"
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+void inputBasicInfo(T &t){
+    string str;
+    char gen;
+    int num;
+    double num1;
+
+    cout << "***************** Employee " << t.getNumEmployees() << " *****************" << endl;
+    cout << "Please enter your First Name: ";
+    cin >> str;
+    t.setFirstName(str);
+    cout << "Please enter your Last Name: ";
+    cin >> str;
+    t.setLastName(str);
+    cout << "Please enter your Gender: ";
+    cin >> gen;
+    t.setGender(gen);
+    cout << "Please enter your Dependents: ";
+    cin >> num;
+    t.setDependents(num);
+
+    Benefit ben;
+    cout << "Please enter your Health Insurance: ";
+    cin >> str;
+    ben.setHealthinsurance(str);
+    cout << "Please enter your Life Insurance: ";
+    cin >> num1;
+    ben.setLifeinsurance(num1);
+    cout << "Please enter your Vacation Days: ";
+    cin >> num;
+    ben.setVacation(num);
+    t.setBenefit(ben);
+}
+
+int main() {
+
+    Benefit ben3("fff", 30000, 10);
+    Salaried sal1("John", "Johnson", 'M', 2, ben3, 1);
+    sal1.setAnnualSalary(75000);
+    cout << endl << "***************** Employee " << sal1.getNumEmployees() << " *****************" << endl;
+    sal1.displayEmployee();
+
+    double annualSalary;
+    Employee emp;
+    inputBasicInfo(emp);
+    cout << "Please enter your AnnualSalary: ";
+    cin >> annualSalary;
+    emp.setAnnualSalary(annualSalary);
+    emp.displayEmployee();
+
+
+    Salaried sal;
+    inputBasicInfo(sal);
+    cout << "Please enter your AnnualSalary: ";
+    cin >> annualSalary;
+    sal.setAnnualSalary(annualSalary);
+    sal.setManLevel(2);
+    sal.displayEmployee();
+
+
+    Hourly hou;
+    inputBasicInfo(hou);
+    hou.setHours(50);
+    hou.setWage(30);
+    hou.setAnnualSalary();
+    hou.displayEmployee();
+
+    Benefit ben5("ppo", 80000, 2);
+    Hourly hou2("Nick", "Stivenson", 'm', 6, ben5,
+        12, 70, "full time");
+    hou2.setAnnualSalary();
+    hou2.displayEmployee();
+
+	system("pause");
+    return 0;
+}
